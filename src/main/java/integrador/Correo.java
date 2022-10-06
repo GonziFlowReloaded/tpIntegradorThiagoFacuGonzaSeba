f77157eda6c36cfe75d59c576007ef65686e8722
Facundo Almaraz <facundoalmaraz8@gmail.com>
2022-10-05 23:23:48 -0300

    Primer Version

---
+++ src/main/java/integrador/Correo.java
@@ -0,0 +1,57 @@
+package integrador;
+
+import java.util.*;
+
+public class Correo {
+    private String contenido;
+    private String asunto;
+    private Usuario remitente;
+    ArrayList<Contacto> para = new ArrayList<Contacto>();
+
+    public Correo(String contenido, String asunto, Usuario remitente, Contacto para) {
+        this.contenido = contenido;
+        this.asunto = asunto;
+        this.remitente = remitente;
+        this.para.add(para);
+
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public String getAsunto() {
+        return asunto;
+    }
+
+    public void setAsunto(String asunto) {
+        this.asunto = asunto;
+    }
+
+    public Usuario getRemitente() {
+        return remitente;
+    }
+
+    public void setRemitente(Usuario remitente) {
+        this.remitente = remitente;
+    }
+
+    public ArrayList<Contacto> getPara() {
+        return para;
+    }
+
+    public void setPara(ArrayList<Contacto> para) {
+        this.para = para;
+    }
+
+    @Override
+    public String toString() {
+        return "Correo{" + "contenido=" + contenido + ", asunto=" + asunto + ", remitente=" + remitente + ", para=" + para + '}';
+    }
+
+    
+}
