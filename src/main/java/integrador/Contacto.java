23e581b1a50664e9589c08468ab9e7d9f66bd2f3
Jose Gonzalo Scali <gonzalojoses@hotmail.com>
2022-10-05 23:35:18 -0300

    Agregue datos al contacto

--- src/main/java/integrador/Contacto.java
+++ src/main/java/integrador/Contacto.java
@@ -1,5 +1,43 @@
 package integrador;
 
 public class Contacto {
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    public Contacto(String nombre, String apellido, String email) {
+        setNombre(nombre);
+        setApellido(apellido);
+        setEmail(email);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacto{" + "nombre=" + nombre + ", apellido=" + apellido + ", email=" + email + '}';
+    }
     
 }
