44b78751e0c5f015a3ab14e5cc53319575b8fb4f
Jose Gonzalo Scali <gonzalojoses@hotmail.com>
2022-10-05 23:40:51 -0300

    update usuario

--- src/main/java/integrador/Usuario.java
+++ src/main/java/integrador/Usuario.java
@@ -2,4 +2,29 @@ package integrador;
 
 public class Usuario {
     
+
+    private String usuario;
+    private String password;
+
+    public Usuario(String usuario, String password) {
+        setUsuario(usuario);
+        setPassword(password);
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
 }
