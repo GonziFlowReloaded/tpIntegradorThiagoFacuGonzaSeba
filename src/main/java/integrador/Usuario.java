71ad1ad539ec880c5f34064cc6fd1461f5e7309d
Facundo Almaraz <facundoalmaraz8@gmail.com>
2022-10-05 23:44:22 -0300

    Agregando mas campos en la clase Usuario

--- src/main/java/integrador/Usuario.java
+++ src/main/java/integrador/Usuario.java
@@ -5,10 +5,14 @@ public class Usuario {
 
     private String usuario;
     private String password;
+    private String nombre;
+    private String email;
 
-    public Usuario(String usuario, String password) {
+    public Usuario(String usuario, String password, String nombre, String email) {
         setUsuario(usuario);
         setPassword(password);
+        setNombre(nombre);
+        setEmail(email);
     }
 
     public String getUsuario() {
@@ -25,6 +29,22 @@ public class Usuario {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
     
 }
